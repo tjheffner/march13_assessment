75cc1347e28ddc8cbd33f94ddb26b0276e166cce
Tanner Heffner <tannerjheffner@gmail.com>
2015-03-13 10:20:51 -0700

    Add necessary lines for twig to work in app.php

--- app/app.php
+++ app/app.php
@@ -10,7 +10,17 @@
   ));
 
   $app->get("/", function() use($app) {
-    return "home";
+    return $app['twig']->render('input.twig');
+  });
+
+  $app->post("/results", function() use($app) {
+
+    $count = new RepeatCounter;
+    $input1 = $_POST['input1'];
+    $input2 = $_POST['input2'];
+    $wordcount = $count->countRepeats($input1, $input2);
+
+    return $app['twig']->render('results.twig', array('matches' => $wordcount));
   });
 
   return $app;
