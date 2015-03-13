b27b736ce1225792e6812d591e08bf31b3d19882
Tanner Heffner <tannerjheffner@gmail.com>
2015-03-13 09:32:25 -0700

    Pass first test

--- src/Counts.php
+++ src/Counts.php
@@ -4,7 +4,14 @@
   {
     function countRepeats($input1, $input2)
     {
+        $counter = 0;
+        $sentence = explode(" ", $input2);
 
+        if (in_array($input1, $sentence)) {
+          $counter++;
+        }
+
+        return $counter;
     }
 
   }
